8f46a3169758df343f4af0082f7518b210da9bbc
James Cuzella <james.cuzella@lyraphase.com>
2023-08-09 22:17:59 -0600

    pytest: Auto-configure tests/* subdirs as markers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,44 @@
 import os
+import pathlib
 import sys
 
+import git
 import pytest
 
 from waybar_check_gmail.util import XDGBaseDirs
 
 # from pytest_mock import mocker
+
+
+# Automatically mark tests by path below top-level 'tests' dir
+def pytest_collection_modifyitems(config, items):
+    # python 3.4/3.5 compat: rootdir = pathlib.Path(str(config.rootdir))
+    rootdir = pathlib.Path(config.rootdir)
+    for item in items:
+        rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
+        mark_name = next((part for part in rel_path.parts if (part != "tests")), "")
+        # mark_name = next((part for part in rel_path.parts), "")
+        if mark_name:
+            mark = getattr(pytest.mark, mark_name)
+            item.add_marker(mark)
+
+
+# Automatically configure pytest marks based on 'tests/' dir structure
+def pytest_configure(config):
+    rootdir = pathlib.Path(config.rootdir)
+    repo = git.Repo(rootdir)
+    # Get first subdir under rootdir matching "tests"
+    _tests_dir = [_dir for _dir in rootdir.glob("tests")][0]
+    # Get all first-level subdirs
+    _tests_subdirs = [_subdir for _subdir in _tests_dir.glob("*") if (_subdir.is_dir())]
+    _paths_considered = [
+        d for d in _tests_subdirs if not str(d) in repo.ignored(_tests_subdirs)
+    ]
+    repo.close()
+    # Create each subdir name as a pytest mark
+    for _path in _paths_considered:
+        mark_name = _path.stem
+        config.addinivalue_line("markers", f"{mark_name}: {mark_name.capitalize()} tests")
 
 
 @pytest.fixture
